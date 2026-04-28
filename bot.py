cb712abd6b36b9a2167f7387c2ec809fe1fb46d5
7alawani93 <mutasim_14@hotmail.com>
2026-04-28 15:32:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,28 +8,22 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 async def handle_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message
 
-    if not msg or not msg.document:
-        return
+    if msg and msg.document:
+        if msg.document.file_name.lower().endswith(".pdf"):
 
-    file_name = msg.document.file_name or ""
+            await context.bot.copy_message(
+                chat_id=CHANNEL_ID,
+                from_chat_id=msg.chat_id,
+                message_id=msg.message_id
+            )
 
-    if not file_name.lower().endswith(".pdf"):
-        return
-
-    # Copy to channel
-    await context.bot.copy_message(
-        chat_id=CHANNEL_ID,
-        from_chat_id=msg.chat_id,
-        message_id=msg.message_id
-    )
-
-    # Delete from group
-    await context.bot.delete_message(
-        chat_id=msg.chat_id,
-        message_id=msg.message_id
-    )
+            await context.bot.delete_message(
+                chat_id=msg.chat_id,
+                message_id=msg.message_id
+            )
 
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(MessageHandler(filters.Document.ALL, handle_pdf))
 
 app.run_polling()
